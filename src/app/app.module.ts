51358c274fddc85e14189dd2040bf8bfca9dbe69
Hachikoi-the-creator <facka72@gmail.com>
2023-03-26 02:06:01 -0500

    add custom api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,12 +14,10 @@ import { TasksListComponent } from './components/tasks-list/tasks-list.component
     HeaderComponent,
     ButtonComponent,
     TaskComponent,
-    TasksListComponent
+    TasksListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task';
 import { TASK } from '../taskHardCodded';
@@ -7,9 +8,34 @@ import { TASK } from '../taskHardCodded';
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private BASE_URL = 'https://crashing-together.onrender.com';
+  constructor(private ngFetch: HttpClient) {}
 
   getAllTasks(): Observable<Task[]> {
-    return of(TASK);
+    return this.ngFetch.get<Task[]>(`${this.BASE_URL}/angular`);
   }
+
+  getOneTask(id: number): Task | {} {
+    return TASK.find((e) => e.id === id) || {};
+  }
+
+  removeTask(id: number): Task {
+    const removed = TASK.splice(id, 1);
+    return removed[0];
+  }
+
+  // addTask(text: string, day: string, reminder: boolean): Task {
+  //   const rndId = Math.round(Math.random() * 100);
+  //   const newTask: Task = { text, day, reminder, id: rndId };
+
+  //   TASK.push(newTask);
+  //   return newTask;
+  // }
+
+  // updateTask(text: string, day: string, reminder: boolean, id: number): Task {
+  //   const updatedTask: Task = { text, day, reminder, id };
+
+  //   TASK.splice(id, 1, updatedTask);
+  //   return updatedTask;
+  // }
 }
